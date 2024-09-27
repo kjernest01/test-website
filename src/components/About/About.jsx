0d0c29235e3fe9b3f039a51a3a6574b5270dcc8a
Jonathan Kopuri <kjernest01@gmail.com>
2024-09-27 12:31:21 -0500

    updated about profile pic

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import './About.css';
 import theme_pattern from '../../assets/theme_pattern.svg'
-import profile_img from '../../assets/vikram_ratan_about_profile.svg'
+import profile_img from '../../assets/vikram_ratan_about_profile.jpg'
 
 const About = () => {
     return (
